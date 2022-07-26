11d5cb478afd64218c75c9d0047945a1d5ebbd34
preml10 <65588305+preml10@users.noreply.github.com>
2022-07-26 17:03:54 +0530

    Add files via upload

---
+++ DAY1_T1/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Day1_T1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Day1_T1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Employee_Page()
+        {
+            return View();
+        }
+
+        public IActionResult Depts_Page()
+        {
+            return View();
+        }
+        public IActionResult Contact_Us()
+        {
+            return View();
+        }
+        public IActionResult About_Us()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
